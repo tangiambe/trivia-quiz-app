7a3e6c636322578ce023fc804f0d5365ea97e0c2
Albert <albertzeap@gmail.com>
2023-08-24 07:50:53 -0700

    Created offCanvas toggle menu for navbar

--- frontend/trivia-app/src/components/Navbar.js
+++ frontend/trivia-app/src/components/Navbar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { OffcanvasHeader, OffcanvasTitle } from "react-bootstrap";
 
 
 
@@ -13,18 +15,27 @@ export const NavBar = () => {
            <Navbar expand="lg" className="bg-body-tertiary" >
             <Container>
                 <Navbar.Brand href="/"><strong>TriviYay!</strong></Navbar.Brand>
-                <Nav>
-                    <Nav.Item className="px-3 py-2">
-                        <Nav.Link href="/signup">
-                            Register
-                        </Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item className="px-3 py-2">
-                        <Nav.Link href="/login">
-                            Login
-                        </Nav.Link>
-                    </Nav.Item>
-                </Nav>
+                <Navbar.Toggle id="offcanvasNavbarToggle" className="border border-0" aria-controls="offcanvasNavbar-expand-lg"/>
+                <Navbar.Offcanvas id="offcanvasNavbar-expand-lg" aria-labelledby="offcanvasNavbarLabel-expand-lg" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">Menu</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1">
+                            <Nav.Item className="px-3 py-2">
+                                <Nav.Link href="/signup">
+                                    <strong>Register</strong>
+                                </Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item className="px-3 py-2">
+                                <Nav.Link href="/login">
+                                    <strong>Login</strong>
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
            </Navbar>
 
